a4516b3f0cd38c6cc6f216231ae6aab83d6d56bf
Sarmad Al Kinany <draskinany@gmail.com>
2024-09-12 15:50:21 +0200

    channel set up

--- main.go
+++ main.go
@@ -16,26 +16,26 @@ func main() {
 		"https://www.php.net/",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		//fmt.Println(link)
-		go checkLink(link)
+		go checkLink(link, c)
 	}
 
-	//fmt.Println("Number of CPUs:", runtime.NumCPU()) //This will print the number of CPU cores available on your system.
-	//fmt.Println("GOMAXPROCS", runtime.GOMAXPROCS(0)) //This will print the current GOMAXPROCS value, which is the number of CPU cores Go is currently using.
-	//The value won't be set to 0; it will simply return the number of OS threads Go is currently using (which, by default, is set to the number of available CPU cores). If you want to change it, you need to pass a non-zero value to GOMAXPROCS.
-	//return
+	fmt.Println(<-c)
+
+	//fmt.Println("Number of CPUs:", runtime.NumCPU()) //This will print the number of CPU cores available on your system
+	//fmt.Println("GOMAXPROCS", runtime.GOMAXPROCS(0)) //This will print the current GOMAXPROCS value
 }
 
-// silly function that does not care about the response,
-// but rather focuses on whether there is an error or not
-// when performing an HTTP request
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down, I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep, its up"
 }
